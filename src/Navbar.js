b1057fbb0bb3bc8668faaa4155879a17ae47e275
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-01-24 12:20:46 +1100

    Header changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,1 +1,11 @@
 import React from "react";
+import icon from "../public/images/globe-icon.png";
+
+export default function Navbar() {
+  return (
+    <div className="header">
+      <img src={icon} className="top-logo" />
+      <h5 className="top-heading">my travel journal</h5>
+    </div>
+  );
+}
