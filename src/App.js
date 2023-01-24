7fe351438afa9016b169b714537ada5da59859dc
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-01-24 14:29:24 +1100

    added props and data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "../index.css";
+import Card from "./Card";
+import data from "./data";
 
-const App = () => {
-  return <Navbar />;
-};
-
-export default App;
+export default function App() {
+  const cardElements = data.map((data) => {
+    return (
+      <Card
+        image={data.imageUrl}
+        country={data.location}
+        address={data.googleMapsUrl}
+        title={data.title}
+        startDate={data.startDate}
+        endDate={data.endDate}
+        description={data.description}
+      />
+    );
+  });
+  return (
+    <div className="page">
+      <Navbar />
+      <div className="card-wrapper">{cardElements}</div>
+    </div>
+  );
+}
